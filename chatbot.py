f0f50f4b2469168bbbab0b82513be0abeb735f73
prakshaaljain <97468349+prakshaaljain@users.noreply.github.com>
2026-08-07 02:25:24 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify 
+from flask import Flask, render_template, request, jsonify  
 import pandas as pd
 import numpy as np
 import pickle as pk
